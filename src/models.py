f6ecb79db1ac4409a379af8176efdbcc1877d320
Giiid <gideonfummey1@gmail.com>
2024-03-12 14:43:57 +0000

    appointment table

--- src/models.py
+++ src/models.py
@@ -11,6 +11,7 @@ class Doctor(db.Model):
     specialization = db.Column(db.String(15), unique=True, nullable=False)
     location = db.Column(db.String(50))
     # patients = db.relationship("Patient", backref="patient", lazy=True)
+    doc_appointment_id = db.Column(db.Integer, db.ForeignKey('appointment.id'))
 
     def __repr__(self):
         return f"Doctor({self.name}, {self.email})"
@@ -23,8 +24,9 @@ class Patient(db.Model):
     email = db.Column(db.String(100), unique=True, nullable=False)
     age = db.Column(db.Integer)
     password = db.Column(db.String(10), unique=True, nullable=False)
-    # doc_id = db.Column(db.Integer, db.Foreignkey("doctor.id"))
+    patient_appointment_id = db.Column(db.Integer, db.ForeignKey('appointment.id'))
     # doc_id = db.Column(db.Integer, db.ForeignKey('doctor.id'))
+
     # type_of_sickness = db.Column(db.String(100))
     # appointed_date = db.Column(db.Date)
 
@@ -33,9 +35,12 @@ class Patient(db.Model):
     
 
 
-class Appointment(Patient):
+class Appointment(db.Model):
     type_of_sickness = db.Column(db.String(100))
     appointed_date = db.Column(db.Date)
+    doctor_appointed = db.relationship("Doctor", backref="doctor", lazy=True)
+    patient_appointed = db.relationship("Patient", backref="patient", lazy=True)
+
 
     def __repr__(self):
         return f"{self.full_name} - {self.type_of_sickness}"
